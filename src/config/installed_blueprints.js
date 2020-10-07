4591ecf13f3a51612cc5217c29b0df3d346656ac
Crowdbotics <team@crowdbotics.com>
2020-10-07 07:58:55 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,6 +1,5 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
-{ name: 'BlankScreen111841', human_name: 'Blank Screen 1', access_route: 'BlankScreen111841'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
